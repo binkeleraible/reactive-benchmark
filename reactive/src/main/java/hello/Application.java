0cebe5d2c22c2674cce25c02193b92def296ab66
dabi <binkele-raible@beontra.com>
2019-02-23 06:33:02 +0100

    setting up reactive webapp with mongodb

--- reactive/src/main/java/hello/Application.java
+++ reactive/src/main/java/hello/Application.java
@@ -3,12 +3,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
-@EnableMongoRepositories
 public class Application {
 
 	public static void main(String[] args) {
--- reactive/src/main/java/hello/PersonController.java
+++ reactive/src/main/java/hello/PersonController.java
@@ -10,8 +10,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -19,9 +17,6 @@ public class PersonController {
 
     @Autowired
     private ReactivePersonRepository repository;
-
-    @Autowired
-    private PersonRepository personRepository;
 
     @RequestMapping(value = "/rperson", method = RequestMethod.GET)
     public Flux<Person> getAllBosses() {
@@ -44,28 +39,4 @@ public class PersonController {
         return this.repository.save(person);
     }
 
-
-
-    @RequestMapping(value = "/person", method = RequestMethod.GET)
-    public List<Person> getAllBosses2() {
-        return this.personRepository.findAll();
-    }
-
-    @RequestMapping(value = "/person/{id}", method = RequestMethod.GET)
-    public Optional<Person> getBossById2(@PathVariable("id") String id) {
-        return this.personRepository.findById(id);
-    }
-
-
-    @RequestMapping(value = "/person/starts/{prefix}", method = RequestMethod.GET)
-    public List<Person> getAllBossesStartsWith2(@PathVariable("prefix") String prefix) {
-        return this.personRepository.findByLastNameStartsWith(prefix);
-    }
-
-
-    @RequestMapping(value = "/person", method = RequestMethod.POST)
-    public @Valid Person createBoss2(@Valid @RequestBody Person person) {
-        person.setId(UUID.randomUUID().toString());
-        return this.personRepository.save(person);
-    }
 }
